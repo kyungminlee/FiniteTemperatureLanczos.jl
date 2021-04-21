e5376a204129d7480d21e8833209ea70ab2280cf
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2021-04-20 21:25:29 -0400

    Update example/spinchain.jl

--- example/spinchain.jl
+++ example/spinchain.jl
@@ -8,12 +8,9 @@ using Plots
 using FiniteTemperatureLanczos
 
 function main()
-
     global temperatures = 0.0:0.1:4
-
     n = 8
     (hs, spin) = QuantumHamiltonian.Toolkit.spin_system(n, 1//2)
-
     hamiltonian = NullOperator()
     for i in 1:n
         j = mod(i, n) + 1
@@ -31,6 +28,7 @@ function main()
     krylovdim = min(D, 50)
     zs = zeros(Float64, (R, length(temperatures)))
     Es = zeros(Float64, (R, length(temperatures)))
+    E2s = zeros(Float64, (R, length(temperatures)))
     @showprogress for r in 1:R
         v = rand(ComplexF64, D) * 2 .- (1 + im)
         normalize!(v)
@@ -39,23 +37,28 @@ function main()
         factorization = initialize(iterator)
         for _ in 1:krylovdim
             expand!(iterator, factorization)
-            if 
         end
-        sm = premeasurestatic(factorization)
+        pm = premeasure(factorization)
+        pre_z  = premeasure(pm, 0)
+        pre_E  = premeasure(pm, 1)
+        pre_E2 = premeasure(pm, 2)
+
         for (it, t) in enumerate(temperatures)
-            (z, E, o) = measure(sm, t)
-            zs[r, it] = z
-            Es[r, it] = E
+            zs[r, it]  = measure(pre_z,  pm.eigen.values, t)
+            Es[r, it]  = measure(pre_E,  pm.eigen.values, t)
+            E2s[r, it] = measure(pre_E2, pm.eigen.values, t)
         end
     end
 
     global Eavgs = vcat(collect(transpose(sum(Es, dims=1) ./ sum(zs, dims=1)))...)
-    h = Matrix(hamiltonian_rep)
-    global Eavgs_true = [    minimum(eigvals(Hermitian(h)))    ]
-    append!(Eavgs_true, tr(h * exp(-Hermitian(h ./ t))) / tr(exp(-Hermitian(h ./ t)))  for t in temperatures[2:end])
-    plot(temperatures, Eavgs, label="FTLM")
-    plot!(temperatures, Eavgs_true, label="Direct")
-    return Eavgs
+    global E2avgs = vcat(collect(transpose(sum(E2s, dims=1) ./ sum(zs, dims=1)))...)
+    # h = Matrix(hamiltonian_rep)
+    # global Eavgs_true = [    minimum(eigvals(Hermitian(h)))    ]
+    # append!(Eavgs_true, tr(h * exp(-Hermitian(h ./ t))) / tr(exp(-Hermitian(h ./ t)))  for t in temperatures[2:end])
+    # plot(temperatures, Eavgs, label="FTLM")
+    plot(temperatures, (E2avgs - Eavgs.^2) ./ temperatures.^2 ./ n, label="FTLM", xlims=(0, 2))
+    # plot!(temperatures, Eavgs_true, label="Direct")
+    # return Eavgs
 end
 
 main()
